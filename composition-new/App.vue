0a13bc3e3f5ea745f65ba5f3176a28cd353fbd05
Kevin Barker <kwkbarker@gmail.com>
2021-12-03 11:41:18 -0500

    begin microblog

---
+++ composition-new/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <button @click="a++">{{ a }}</button>
+  <button @click="b++">{{ b }}</button>
+  <p>{{ total }}</p>
+  <div v-for="number in history"
+    :key="number"
+  >
+    {{ number }}
+  </div>
+</template>
+<script>
+import { useNumbers } from './useNumbers.js'
+
+export default {
+  setup() {
+    return useNumbers()
+  }
+}
+</script>
+
+<style scoped>
+p {
+  font-size: 40px;
+}
+button {
+  height: 100px;
+  width: 100px;
+  font-size: 40px;
+}
+</style>
